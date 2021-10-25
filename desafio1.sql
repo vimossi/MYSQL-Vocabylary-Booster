a43b59519f2db03dd577be26aa850f2b900a6d73
ViniciusMossi <vini_agg@hotmail.com>
2021-10-25 18:45:10 -0400

    reformula codigo, nao estavam passando

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,5 @@
-SELECT COUNTRY_NAME AS 'País', IF(REGION_ID = 1, "incluido", "não incluido")AS "Status Inclusão" FROM hr.countries
-ORDER BY COUNTRY_NAME ASC
+SELECT
+  COUNTRY_NAME AS País, IF(REGION_ID = 1,
+  'incluído', 'não incluído') AS `Status Inclusão`
+FROM hr.countries ORDER BY País;
+
--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,13 @@
-SELECT job.JOB_TITLE AS 'Cargo',
-ROUND(AVG(employee.SALARY), 2) AS 'Média salarial',
-CASE 
-WHEN ROUND(AVG(employee.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-WHEN ROUND(AVG(employee.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-WHEN ROUND(AVG(employee.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+SELECT
+trabalho.JOB_TITLE AS Cargo,
+ROUND(AVG(empregado.SALARY), 2) AS 'Média salarial',
+CASE
+WHEN ROUND(AVG(empregado.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN ROUND(AVG(empregado.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN ROUND(AVG(empregado.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
 ELSE 'CEO'
-END AS 'Senioridade'
-FROM hr.employees AS employee
-JOIN hr.jobs AS job ON job.JOB_ID = employee.JOB_ID
-GROUP BY employee.JOB_ID
-ORDER BY 'Média salarial', Cargo;
+END AS Senioridade
+FROM hr.employees AS empregado
+JOIN hr.jobs AS trabalho ON trabalho.JOB_ID = empregado.JOB_ID
+GROUP BY empregado.JOB_ID
+ORDER BY `Média salarial`, Cargo;
