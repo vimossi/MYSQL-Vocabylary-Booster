40c5f2bec6870bf3765975ba4d3075768e2dcce3
ViniciusMossi <vini_agg@hotmail.com>
2021-10-26 12:52:29 -0400

    implementa req 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+SELECT 
+CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome compĺeto',
+J.JOB_TITLE AS 'Cargo',
+S.START_DATE AS 'Data de início do cargo',
+D.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.job_history S
+JOIN hr.employees E ON S.EMPLOYEE_ID = E.EMPLOYEE_ID
+JOIN hr.jobs J ON J.JOB_ID = S.JOB_ID
+JOIN hr.departments D ON D.DEPARTMENT_ID = S.DEPARTMENT_ID
+ORDER BY 'Nome completo' DESC, 'Cargo';
