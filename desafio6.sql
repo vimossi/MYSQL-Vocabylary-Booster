1534b90c60de14f57dd7952c8c915c207917f2db
ViniciusMossi <vini_agg@hotmail.com>
2021-10-26 13:02:05 -0400

    verifica se passou pois no workbanch funcionou ok

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 SELECT 
-CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome compĺeto',
+CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
 J.JOB_TITLE AS 'Cargo',
 S.START_DATE AS 'Data de início do cargo',
 D.DEPARTMENT_NAME AS 'Departamento'
