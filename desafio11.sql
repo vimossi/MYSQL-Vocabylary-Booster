cee5946ea2a13f5a21f9f8033cab11d3f392225f
ViniciusMossi <vini_agg@hotmail.com>
2021-10-26 21:51:22 -0400

    verifica erro por nao passar evaluetor

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,10 @@
-SELECT compatriot.contactName AS 'Nome',
-compatriot.country AS 'País',
-COUNT(compatriot.country) AS 'Número de compatriotas'
-FROM w3schools.customers AS compatriot
+SELECT 
+CO.contactName AS `Nome`,
+CO.country AS `País`,
+COUNT(CO.country) AS `Número de compatriotas`
+FROM w3schools.customers AS CO
 JOIN w3schools.customers AS countries
-ON compatriot.country = countries.country
-AND compatriot.customerID <> countries.customerId
-GROUP BY 'Nome', countries.country
-ORDER BY 'Nome';
+ON CO.country = countries.country
+AND CO.customerID <> countries.customerId
+GROUP BY `Nome`, countries.country
+ORDER BY `Nome`;
--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,4 @@ employees.salary AS 'Salário'
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS job_history ON job_history.employee_id = employees.EMPLOYEE_ID
 WHERE MONTH(job_history.start_date) BETWEEN 1 AND 3
-ORDER BY 'Nome completo', job_history.START_DATE;
+ORDER BY `Nome completo`, job_history.START_DATE;
